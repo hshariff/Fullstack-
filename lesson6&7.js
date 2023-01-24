7ffddd250da77d59dd238b1365fdf19f75111ae0
Hashim <hashim.mshariff@gmail.com>
2023-01-24 17:23:26 +0000

    syntaxes,datatypes & variables and arrays

---
+++ lesson6&7.js
@@ -0,0 +1,70 @@
+console.log("Just practing some  js code")
+
+//var whoAmI = " Hashim"
+
+const name= "Hashim"
+
+var age = " I am 23 years of age"
+
+// string concatenation eg.
+   console.log("My name is " + name, "and" +  age  )
+
+ //console.log(whoAmI)
+ //console.log(age)
+
+//concatenation
+
+//number concatenation 
+
+console.log(2+4+10+20) 
+
+//more variables
+
+//var today= "Tuesday"
+
+time= "12:00"
+
+//let changes
+let tomorrow= "Wednesday"
+console.log(tomorrow)
+
+//constant is used, never changes
+const Name= "Hashim"
+
+tomorrow = "Friday"
+console.log(tomorrow)
+tomorrow = "Saturday"
+console.log(tomorrow)
+
+console.log(time)
+
+//console.log(typeof tomorrow) //type of shows u which datatype the var belongs to
+
+//  different types of datatypes
+    // string (""), number, object, underfined (;) & null
+
+
+//covering Arrays []
+
+function obj(){
+    //
+}
+
+var fruits = ["banana", "apple", "orange",1,3,4,3,3,2,2,4,23,2,232,42] //an array can hold all datatypes<
+fruits[0]
+console.log("the number of items in the list is" ,fruits.length) //length will show u how many items in the array
+
+//console.log(fruits[14])
+fruits.pop() //pop always removes the last item
+//console.log("the number of items in the list is" ,fruits.length)
+fruits.push("banana")
+//console.log("the number of items in the list is" ,fruits.length)
+
+//reassigning items in the list 
+
+fruits[1]= "pear"
+fruits[0] = "grapefruit"
+fruits[fruits.length-2] = "apples" 
+console.log(fruits)
+
+
--- lesson6.js
+++
@@ -1,8 +0,0 @@
-console.log("Just practing some very basic js code")
-
-var whoAmI = " My name is Hashim"
-
-var age = "I am 23 years of age"
-
-console.log(whoAmI)
-console.log(age)
