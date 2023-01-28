6813cbc8bc0a7dcbb34cd79becfbe385ebc9b8e6
Hashim <hashim.mshariff@gmail.com>
2023-01-28 01:04:14 +0000

    conditional statements

---
+++ lesson8.js
@@ -0,0 +1,35 @@
+//conditional statments 
+
+// boolean - another data type - consist of true or false ---case sensitive
+
+//console.log(true)
+
+//var name = "Hashim"
+
+/* An example --
+
+if (late){
+     get up
+} else (late){
+    eat breakfast
+} 
+else if {
+     chill in bed
+}
+*/
+
+var Name ="Mohamed"
+ 
+if (Name === "Hashim"){
+    console.log(`hello ${Name}! how are you doing?`)
+}
+else if(Name==="Mohamed"){
+    console.log(`hello ${Name}! how are you doing?`)
+}
+else if (Name==="Ahmed"){
+    console.log(`hello ${Name}! how are you doing?`)
+}
+else {
+    console.log("sorry, idk u")
+}
+ 
